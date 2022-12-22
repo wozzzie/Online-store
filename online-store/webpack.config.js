3f7e38e66888b8890b22e553ea2ae275b0088f54
Karina Koldaeva <71209378+KarinaKold@users.noreply.github.com>
2022-12-22 21:37:41 +0300

    feat: add settings

--- online-store/webpack.config.js
+++ online-store/webpack.config.js
@@ -2,17 +2,28 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const EslingPlugin = require('eslint-webpack-plugin');
 
 const baseConfig = {
     entry: path.resolve(__dirname, './src/index'),
     mode: 'development',
     module: {
         rules: [
-            { test: /\.ts$/i, use: 'ts-loader' },
             {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+            },
+            {
+                test: /\.ts$/i,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
             },
         ],
     },
@@ -24,7 +35,6 @@ const baseConfig = {
         path: path.resolve(__dirname, '../dist'),
     },
     plugins: [
-        new EslingPlugin({ extensions: 'ts' }),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './src/index.html'),
             filename: 'index.html',
